8995e0d4e308f35e1c2a3cad4d247f630c104d58
Singaravelan <singaravelan4420@gmail.com>
2026-01-05 00:08:44 +0530

    Modified FindDuplicates in Array

--- ArraysPrograms/DuplicateElementsInArray.java
+++ ArraysPrograms/DuplicateElementsInArray.java
@@ -1,45 +1,37 @@
-public class DuplicateElementsInArray{
+public class DuplicateElementsInArray
+{
+    public static void main(String[] args)
+    {
+        String[] a = {"JAVA", "C", "C++", "JAVA", "Python", "Ruby", "Python", "JAVA"};
 
-	public static void main(String[] args)
-	 {
-	 
-		String a[] = {"JAVA", "C", "C++", "JAVA", "Python", "Ruby", "Python"};
-		int n = a.length;
-		Boolean isDuplicate = false;
-		
-		
-		
-		for(int i=0; i<n; i++)
-		 {
-		 
-			for(int j=i+1; j<n; j++)
-			 {
-			 
-				if(a[i]==a[j])
-				 
-				 {
-				 
-					
-					System.out.println( "The Duplicate elements  are " + a[i]);
-					isDuplicate = true;
-				 
-				 
-				 }
-			 
-			 
-			 
-			 
-			 }
-		 
-		 
-		 }
-		  if(isDuplicate==false)
-			{
-				System.out.println("There is no duplicate elements in the array");
-			}
- 
-	 
-	 }
+        System.out.println("Duplicate elements in the array:");
 
+        for (int i = 0; i < a.length; i++)
+        {
+            boolean isDuplicate = false;
 
+            // Check if this element already appeared before index i
+            for (int k = 0; k < i; k++)
+            {
+                if (a[i].equals(a[k]))
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (isDuplicate)
+                continue;
+
+            // Compare with remaining elements
+            for (int j = i + 1; j < a.length; j++)
+            {
+                if (a[i].equals(a[j]))
+                {
+                    System.out.println(a[i]);
+                    break;
+                }
+            }
+        }
+    }
 }
