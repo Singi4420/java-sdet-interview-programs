27a297e9e786117a8aa99c60eb388f16184909e2
Singaravelan <singaravelan4420@gmail.com>
2026-01-08 05:59:26 +0530

    Refactor PrimeNumber class for clarity and correctness

--- NumberPrograms/PrimeNumber.java
+++ NumberPrograms/PrimeNumber.java
@@ -1,72 +1,33 @@
-public class PrimeNumber{
-
-	public static void main(String[] args){
-	
+public class PrimeNumber
+{
+	public static void main(String[] args)
+	{
 		int num = 34;
-		Boolean isPrime = true;
+		boolean isPrime = true;
 		
-		
-		if(num<=1){
-		
+		if(num<=1)
+		{
 			isPrime = false;
-		
 		}
-		else{
-		
-		
-			for(int i=2; i<=Math.sqrt(num); i++){
-			
-			
-			if(num%2==0){
-			
-			isPrime = false;
-			
-			
-			
-			
-			
+		else
+		{
+			for(int i = 2; i<=Math.sqrt(num); i++)
+			{
+				if(num%i == 0)
+				{
+					isPrime = false;
+					break;
+				}
 			}
-			
-			
-			
-			
-			
-			
-			
-			}
-		
-		
-		
-		
 		}
 		
-		if(isPrime){
-		
-			System.out.println("The Given number " + num + " is a prime number");
+		if(isPrime)
+		{
+			System.out.println("The Given number is a prime number");
 		}
-		else{
-			System.out.println("The Given number " + num + " is not a prime number");	
-		
+		else
+		{
+			System.out.println("The Given Number is not a prime number");
 		}
-	
-	
-	
-	
-	
-	
-	
-	
 	}
-
-
-
-
-
-
-
-
-
-
-
-
 }
