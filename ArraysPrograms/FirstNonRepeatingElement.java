fd59aa87e8605beed6cb0234d7d37501e1073282
Singaravelan <singaravelan4420@gmail.com>
2026-01-08 05:47:31 +0530

    Refactor first non-repeating element logic
    Refactored the logic to find the first non-repeating element in an array by using a more efficient approach with improved variable names.

--- ArraysPrograms/FirstNonRepeatingElement.java
+++ ArraysPrograms/FirstNonRepeatingElement.java
@@ -3,31 +3,43 @@ public class FirstNonRepeatingElementInArray
 	public static void main(String[] args)
 	{
 		int a[] = {10, 20, 10, 30, 40, 20 , 30, 60, 70};
-		boolean found =  false;
+		int n = a.length;
 		
-		for(int i = 0; i < a.length; i++)
+		for(int i=0; i<n; i++)
 		{
+			boolean alreadyChecked = false;
+			
+			for(int j=0; j<i;j++)
+			{
+				if(a[i]==a[j])
+				{
+					alreadyChecked = true;
+					break;
+				}
+			}
+			
+			if(alreadyChecked)
+			{
+				continue;
+			}
+			
 			int count = 0;
 			
-			for(int j = 0; j <a.length; j++)
+			for(int k=0; k<n; k++)
 			{
-				if( a[i] == a[j] )
+				if(a[i] == a[k])
 				{
 					count++;
 				}
 			}
 			
-			if( count == 1 )
+			if(count == 1)
 			{
-				System.out.println("The first non-repeating element is :" + a[i]);
-				found = true;
+				System.out.println("The First Non Repeating Element is : " + a[i]);
 				break;
 			}
 		}
 		
-		if( !found )
-		{
-			System.out.println("There is no non-repeating element in the array");
-		}
+
 	}
 }
