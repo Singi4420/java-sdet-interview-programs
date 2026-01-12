3939bcbdb2c2b73876a88e71f100c5d48e650f29
Singaravelan <singaravelan4420@gmail.com>
2026-01-12 21:41:27 +0530

    Refactor AnagramCheck main method for clarity
    Refactored the anagram check logic to remove redundancy and improve readability.

--- StringPrograms/AnagramCheck.java
+++ StringPrograms/AnagramCheck.java
@@ -1,28 +1,40 @@
 import java.util.Arrays;
-
 public class AnagramCheck
 {
-    public static void main(String[] args)
-    {
-        String s1 = "Dormitory";
-        String s2 = "Dirty room";
+	public static void main(String[] args)
+	{
+		String s1 = "Dormitory";
+		String s2 = "Dirty room";
+		
+		
+		
+		s1 = s1.replace(" ", "").toLowerCase();
+		s2 = s2.replace(" ", "").toLowerCase();
+		if(s1.length()!=s2.length())
+		{ 
+			System.out.println("the Given String are not anagram");
+            return;
+		}
+		
+		else 
+		{
+		
+		char[] c1 = s1.toCharArray();
+		char[] c2 = s2.toCharArray();
+		
+		Arrays.sort(c1);
+		Arrays.sort(c2);
+		
+		if(Arrays.equals(c1,c2))
+		{
+			System.out.println("The Given Strings are Anagram");
+		}
+		else
+		{
+			System.out.println("the Given String are not Anagram");
+		}
+		
+		}
 
-        s1 = s1.toLowerCase().replace(" ", "");
-        s2 = s2.toLowerCase().replace(" ", "");
-
-        char[] c1 = s1.toCharArray();
-        char[] c2 = s2.toCharArray();
-
-        Arrays.sort(c1);
-        Arrays.sort(c2);
-
-        if (Arrays.equals(c1, c2))
-        {
-            System.out.println("The given two strings are anagrams of each other");
-        }
-        else
-        {
-            System.out.println("The given two strings are not anagrams");
-        }
-    }
+	}
 }
